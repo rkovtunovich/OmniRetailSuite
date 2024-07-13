1531c6894b18895211d9b7a05c7fcc327de2ceaa
rkovtunovich <roma170586@gmail.com>
2024-07-13 22:00:42 +0300

    Refactor and update package versions
    - Updated package versions in `RetailAssistant.Application.csproj` to stay current with the latest improvements and fixes. This includes updating `AutoMapper` to `13.0.1` and `Microsoft.AspNetCore.Components.WebAssembly.Authentication` to `8.0.7`, ensuring the application benefits from the latest features and bug fixes.
    - The removal of `AutoMapper.Extensions.Microsoft.DependencyInjection` from the project file is in line with the strategy to move away from AutoMapper, streamlining the application's dependencies and potentially improving performance.

--- src/WebApps/RetailAssistant/RetailAssistant.Application/Mapping/Configuration/ConfigureMapping.cs
+++ src/WebApps/RetailAssistant/RetailAssistant.Application/Mapping/Configuration/ConfigureMapping.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace RetailAssistant.Application.Mapping.Configuration;
 
